e31acfa0099226e8240e04babb44343eb00e199a
Potapov Aleksey <alex.potap1811@gmail.com>
2019-06-02 16:26:34 +0300

    Updates

---
+++ static/js/6.7f3c3336.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{60:function(n,e,o){"use strict";o.r(e);var t=o(0),a=o.n(t);e.default=function(){return a.a.createElement("h1",null,"Welcome to Paws online store")}}}]);
+//# sourceMappingURL=6.7f3c3336.chunk.js.map
